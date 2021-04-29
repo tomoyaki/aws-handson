29c852c68be119bb5468b3d7302c817e21407816
tomoyaki <tk5819555@gmail.com>
2021-04-29 16:42:46 +0900

    add Logging library

--- lambdaHello.py
+++ lambdaHello.py
@@ -1,7 +1,13 @@
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    # TODO implement
+    
+    logger.info(event)
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
