c50ea57697ee4f0044baa9cdb5980e67a628d180
tomoyaki <tk5819555@gmail.com>
2021-05-03 15:49:00 +0900

    AWS Translateを呼び出すよう実装を変更

--- serverless#2/translate-function.js
+++ serverless#2/translate-function.js
@@ -1,7 +1,33 @@
+const Aws = require('aws-sdk');
+const Translate = new Aws.Translate({region: 'us-east-1'});
+ 
 exports.handler = async (event) => {
-  const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
+    console.log('event ：' + JSON.stringify(event));
+    let js = JSON.stringify(event);
+    let body = JSON.parse(js);
+
+    let js_body_text = "こんにちは";
+    //日本語から英語に翻訳
+    let rs = await getAwsTranslate(js_body_text,'ja','en');
+
+    return {statusCode: 200,message:rs.TranslatedText};
+};
+ 
+function getAwsTranslate(js_text,in_Language,out_Language) {
+    return new Promise(((resolve, reject) => {
+        let params = {
+            Text: js_text,
+            SourceLanguageCode: in_Language,
+            TargetLanguageCode: out_Language
+        }
+        Translate.translateText(params, function(err,data){
+            if (err) {
+                console.log(err);
+                reject();
+            } else {
+                console.log(JSON.stringify(data));
+                resolve(data);
+            }
+        });
+    }));
 };
