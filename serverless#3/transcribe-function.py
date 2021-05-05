0c631899c63445f2d7398890b1a3d935e3b3440b
tomoyaki <tk5819555@gmail.com>
2021-05-05 12:34:32 +0900

    Amazon Transcribeを呼び出すよう修正

--- serverless#3/transcribe-function.py
+++ serverless#3/transcribe-function.py
@@ -1,22 +1,23 @@
 import json
 import urllib.parse
 import boto3
-
-print('Loading function')
+import datetime
 
 s3 = boto3.client('s3')
-
+transcribe = boto3.client('transcribe')
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-
-    # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        transcribe.start_transcription_job(
+            TranscriptionJobName=datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '_Transcription',
+            LanguageCode='ja-JP',
+            Media={
+                'MediaFileUri': 'https://s3.ap-northeast-1.amazonaws.com/' + bucket + '/' + key
+            },
+            OutputBucketName='h4b-serverless-output-20210505'
+        )
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
